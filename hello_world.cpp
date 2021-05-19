fb8573421abf122e94ba04b63fa4096790f03b7a
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2021-05-19 21:16:42 +0300

    Commit for rebase

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 string name;
-std::getline(std::cin, name); //Ввод строки с пробелами
+std::getline(std::cin, name); //Enter tne name
 cout << "Hello, world from " << name << endl;
 return 0;
 }
